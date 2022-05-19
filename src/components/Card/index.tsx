e9438c995da1601521b7e5799f668ac3b559068c
Vinicius Ribeiro <vayne99@gmail.com>
2022-05-19 10:17:33 -0300

    Implements dig sentence functionality

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,18 +3,20 @@ import './style.css'
 
 interface CardProps {
     content?: String,
-    type: 'number' | 'number-empty' | 'sentence' | 'sentence-empty'
+    type: 'number' | 'number-empty' | 'sentence' | 'sentence-empty',
+    onCardClicked: (type:String) => void
 }
 
 
-export default function Card({ content, type }: CardProps) {
+export default function Card({ content, type, onCardClicked }: CardProps) {
     return (
         <div className="card"
             style={{
                 border: `2px ${type.match(/empty/g)?.toString() === 'empty' ? 'dashed' : 'solid'} 
             ${type.match(/sentence/g)?.toString() === 'sentence' ? 'red' : 'blue'}`,
             backgroundColor: `${(type === 'number-empty' || type === 'sentence-empty') && 'transparent'}`
-            }}>
+            }}
+            onClick={() => onCardClicked(type)}>
             {content}
         </div>
     )
--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -8,12 +8,13 @@ import './style.css'
 
 export function Game() {
 
-  const [sentencesDig, setSentencesDig] = useState<String[]>();
-  const [sentencesPut, setSentencesPut] = useState<String[]>();
+  const [sentencesDig, setSentencesDig] = useState<Sentence[]>([]);
+  const [sentencesPut, setSentencesPut] = useState<Sentence[]>();
   const [numberDig, setNumberDig] = useState<String[]>();
   const [numberPut, setNumberPut] = useState<String[]>();
   const [player1Numbers, setPlayer1Numbers] = useState<String[]>();
   const [player2Numbers, setPlayer2Numbers] = useState<String[]>();
+  const [hasDigged, setHasDigged] = useState(false);
 
 
   useEffect(() => {
@@ -26,24 +27,34 @@ export function Game() {
     setNumberDig([...drawnedNumbers].splice(0, 10));
   }, []);
 
+  const onCardClicked = (type: String) => {
+    if (type === 'sentence' && !hasDigged) {
+      setSentencesPut([sentencesDig[sentencesDig.length-1], ...sentencesDig]);
+      let sentencesDigTemp = sentencesDig;
+      sentencesDigTemp.pop();
+      setSentencesDig(sentencesDigTemp);
+      setHasDigged(true);
+    }
+  }
+
   return (
     <div className="game">
       <div className="cards-deck cards-deck-player2">
-        {player2Numbers?.map((number, id) => <Card content={number} key={id} type='number' />)}
+        {player2Numbers?.map((number, id) => <Card content={number} key={id} type='number' onCardClicked={onCardClicked}/>)}
       </div>
 
       <div className="digs-container">
-        <Card type="sentence" />
+        <Card type="sentence" onCardClicked={onCardClicked}/>
         <div className="cards-put">
-          <Card type={numberPut !== undefined ? 'sentence' : 'sentence-empty'}
-            content={numberPut !== undefined ? numberPut[0] : undefined} />
-          <Card type="number-empty" />
+          <Card type={sentencesPut !== undefined ? 'sentence' : 'sentence-empty'}
+            content={sentencesPut !== undefined ? sentencesPut[0].sentence : undefined} onCardClicked={onCardClicked}/>
+          <Card type="number-empty" onCardClicked={onCardClicked}/>
         </div>
-        <Card type="number" />
+        <Card type="number" onCardClicked={onCardClicked}/>
       </div>
 
       <div className="cards-deck cards-deck-player1">
-        {player1Numbers?.map((number, id) => <Card content={number} key={id} type='number' />)}
+        {player1Numbers?.map((number, id) => <Card content={number} key={id} type='number' onCardClicked={onCardClicked}/>)}
       </div>
     </div>
   )
