40b7689fc4f23e1e55af8d289b1ac9e139d50fca
Vinicius Ribeiro <vayne99@gmail.com>
2022-05-23 12:37:44 -0300

    Make cards layout responsible

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,19 +6,20 @@ interface CardProps {
     type: 'number' | 'number-empty' | 'sentence' | 'sentence-empty',
     onCardClicked: (type: String, id: number, owner: 1 | 2 | undefined) => void,
     id?: number,
-    owner?: 1 | 2 | undefined;
+    owner?: 1 | 2 | undefined,
+    playerTurn?: 1 | 2
 }
 
-export default function Card({ content, type, onCardClicked, id, owner }: CardProps) {
+export default function Card({ content, type, onCardClicked, id, owner, playerTurn }: CardProps) {
 
     return (
-        <div className={`card ${type.includes('sentence') && type.includes('empty') ?
-            'w-24 h-15 md:w-32 md:h-24' : 'w-15 h-24 md:w-24 md:h-32'} 
-        bg-transparent rounded-lg border-2 shadow-m flex flex-col items-center justify-center
-    ${type.includes('empty') && 'border-dashed'}  ${type.includes('number') && !type.includes('empty')&&'bg-numberCardBg'}
-    ${type.includes('sentence') && !type.includes('empty') && 'bg-sentenceCardBg'}`}
+        <div className={`${type.includes('sentence') ? 'w-24 h-16 md:w-32 md:h-24' : 'w-16 h-24 md:w-24 md:h-32'} 
+        bg-transparent rounded-lg border-2 ${!type.includes('empty') && 'shadow-md shadow-black'} flex flex-col items-center justify-center
+    ${type.includes('empty') && 'border-dashed'}  ${type.includes('number') && !type.includes('empty') && 'bg-numberCardBg'}
+    ${type.includes('sentence') && !type.includes('empty') && 'bg-sentenceCardBg'} 
+    ${playerTurn === owner && 'hover:scale-105 transition-transform'}`}
             onClick={() => onCardClicked(type, id as number, owner)}>
-            <p className='font-cursive text-xs md:text-sm lg:text-lg'>{content}</p>
+            <p className='font-cursive text-xs leading-[0.7rem] md:text-sm lg:text-sm'>{content}</p>
         </div>
     )
 }
--- src/components/PlayerDeck/index.tsx
+++ src/components/PlayerDeck/index.tsx
@@ -18,16 +18,17 @@ export function PlayerDeck({ playerTurn, report, changeTurn, hasDiggedNumber,
     hasDiggedSentence, playerNumbers, onCardClicked, owner, numberPut }: PlayerDeckProps) {
     return (
         <div className="flex items-center justify-between w-full px-2 p-0">
-            <div className="w-[20%]">
+            <div className="w-[17%] md:w-[20%]">
                 {playerTurn === owner && !hasDiggedSentence && numberPut.length !== 0 &&
                     <Button onClick={report} type='report'>Denunciar</Button>}
                 {playerTurn === owner && hasDiggedNumber && <Button onClick={changeTurn}>Passar</Button>}
             </div>
-            <div className="flex flex-wrap-reverse w-[50%]">
+            <div className="flex flex-wrap-reverse w-[60%] md:w-[50%]">
                 {playerNumbers?.map((number, id) =>
-                    <Card content={number} key={id} type='number' onCardClicked={onCardClicked} id={id} owner={owner} />)}
+                    <Card content={number} key={id} type='number' onCardClicked={onCardClicked} id={id} 
+                    owner={owner} playerTurn={playerTurn}/>)}
             </div>
-            <div className="w-[20%]">
+            <div className="w-[10%] md:w-[20%]">
                 {playerTurn === owner && <h3 className="text-green font-bold font-sans text-xl">Sua vez!</h3>}
             </div>
         </div>
